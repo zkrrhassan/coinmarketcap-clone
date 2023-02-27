8e0c53bb2803bf785acc9c151127425898042d60
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-27 21:52:03 +0100

    Fixed globalStatistics component

--- src/components/layout/Infobar/GlobalStatistics/GlobalStatistics.tsx
+++ src/components/layout/Infobar/GlobalStatistics/GlobalStatistics.tsx
@@ -20,41 +20,50 @@ interface CoinsGlobal {
 }
 
 const GlobalStatistics = () => {
-	const { data: global } = useQuery({
+	const {
+		data: global,
+		isLoading,
+		isError,
+	} = useQuery({
 		queryKey: ['global'],
 		queryFn: async () => (await axios.get<CoinsGlobal>('/api/global')).data,
 	});
 
+	if (isLoading) return <GlobalStatisticsContainer />;
+
+	if (isError) return <GlobalStatisticsContainer />;
+
+	const {
+		active_cryptocurrencies,
+		market_cap_percentage,
+		markets,
+		total_market_cap,
+		total_volume,
+	} = global;
+
 	return (
-		<>
-			{global && (
-				<GlobalStatisticsContainer>
-					<GlobalStatistic
-						name="Cryptos"
-						value={formatLargeValue(global.active_cryptocurrencies)}
-					/>
-					<GlobalStatistic name="Exchanges" value={global.markets} />
-					<GlobalStatistic
-						name="Market Cap"
-						value={`$${formatLargeValue(global.total_market_cap.usd)}`}
-					/>
-					<GlobalStatistic
-						name="24h Vol"
-						value={`$${formatLargeValue(global.total_volume.usd)}`}
-					/>
-					<GlobalStatistic
-						name="Dominance"
-						value={`BTC: ${formatPercentageValue(
-							global.market_cap_percentage.btc,
-							1
-						)}% ETH: ${formatPercentageValue(
-							global.market_cap_percentage.eth,
-							1
-						)}%`}
-					/>
-				</GlobalStatisticsContainer>
-			)}
-		</>
+		<GlobalStatisticsContainer>
+			<GlobalStatistic
+				name="Cryptos"
+				value={formatLargeValue(active_cryptocurrencies)}
+			/>
+			<GlobalStatistic name="Exchanges" value={markets} />
+			<GlobalStatistic
+				name="Market Cap"
+				value={`$${formatLargeValue(total_market_cap.usd)}`}
+			/>
+			<GlobalStatistic
+				name="24h Vol"
+				value={`$${formatLargeValue(total_volume.usd)}`}
+			/>
+			<GlobalStatistic
+				name="Dominance"
+				value={`BTC: ${formatPercentageValue(
+					market_cap_percentage.btc,
+					1
+				)}% ETH: ${formatPercentageValue(market_cap_percentage.eth, 1)}%`}
+			/>
+		</GlobalStatisticsContainer>
 	);
 };
 
