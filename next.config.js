93f0a760169cf77f214d9c460a72fdbc362d1dd5
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-28 16:42:30 +0100

    Changed images domains

--- next.config.js
+++ next.config.js
@@ -5,13 +5,7 @@ const nextConfig = {
 		styledComponents: true,
 	},
 	images: {
-		domains: [
-			's.yimg.com',
-			'media.wired.com',
-			'cdn.vox-cdn.com',
-			'assets.coingecko.com',
-			'res.cloudinary.com',
-		],
+		domains: ['assets.coingecko.com', 'res.cloudinary.com'],
 		formats: ['image/webp'],
 	},
 	async redirects() {
