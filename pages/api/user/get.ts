1ceddaedf8a0f9ed2f3d29bf8f3cbc56e016b3bf
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-28 16:43:58 +0100

    Updated getUser endpoint

--- pages/api/user/get.ts
+++ pages/api/user/get.ts
@@ -6,22 +6,37 @@ type GetUserRequest = {
 	email: string;
 };
 
+function exclude<User, Key extends keyof User>(
+	user: User,
+	keys: Key[]
+): Omit<User, Key> {
+	for (let key of keys) {
+		delete user[key];
+	}
+	return user;
+}
+
 const getUser: NextApiHandler = async (
 	req: NextApiRequest,
 	res: NextApiResponse
 ) => {
-	const { name, email } = req.query as GetUserRequest;
+	try {
+		const { name, email } = req.query as GetUserRequest;
 
-	const user = await prisma.user.findFirst({
-		where: { name, email },
-	});
+		const user = await prisma.user.findFirst({
+			where: { name, email },
+		});
 
-	if (!user) {
-		return res
-			.status(404)
-			.send({ error: `Couldn't find user with name ${name}` });
+		if (!user) {
+			return res
+				.status(404)
+				.send({ error: `Couldn't find user with name ${name}` });
+		}
+
+		res.status(200).json(exclude(user, ['password']));
+	} catch (error) {
+		console.error(error);
+		res.status(500).send({ message: 'Internal server error' });
 	}
-
-	res.json(user);
 };
 export default getUser;
