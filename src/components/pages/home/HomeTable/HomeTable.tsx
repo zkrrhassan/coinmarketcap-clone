6f0923ad5cecd58fb44b2428792df2589eb51320
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-28 16:46:37 +0100

    updated home table

--- src/components/pages/home/HomeTable/HomeTable.tsx
+++ src/components/pages/home/HomeTable/HomeTable.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 import PercentageChange from 'components/PercentageChange/PercentageChange';
 import WatchlistButton from 'components/WatchlistButton/WatchlistButton';
 import { useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
+import { useState, useMemo } from 'react';
 import { Watchlist } from '@prisma/client';
 import { startsWithHttp } from 'utils/formatLink';
 import { CoinData } from 'pages';
@@ -28,7 +28,7 @@ const HomeTable = ({ initialCoins }: HomeTableProps) => {
 		useState<(CoinOnWatchlist | CoinData)[]>(initialCoins);
 	const { data: session, status } = useSession();
 	const userId = session?.user.id;
-	const { data: watchlistCoins, refetch } = useQuery({
+	const { refetch } = useQuery({
 		queryKey: ['watchlistCoins', status],
 		queryFn: async () => {
 			const data = (
@@ -60,141 +60,151 @@ const HomeTable = ({ initialCoins }: HomeTableProps) => {
 		});
 	};
 
-	const columns: TableColumn<CoinOnWatchlist | CoinData>[] = [
-		{
-			id: 'add-to-watchlist',
-			header: '',
-			cell: ({ row }) => (
-				<WatchlistButton
-					coinId={row.original.id}
-					isOnWatchlist={
-						'isOnWatchlist' in row.original ? row.original.isOnWatchlist : false
-					}
-					watchlistCallback={refetch}
-				/>
-			),
-
-			size: 'auto' as unknown as number,
-		},
-		{
-			header: '#',
-			accessorKey: 'market_cap_rank',
-			size: 50,
-			textAlign: 'start',
-		},
-		{
-			header: 'Name',
-			accessorKey: 'name',
-			cell: ({ row, getValue }) => (
-				<S.LinkWrapper>
-					<Link href={`/coins/${row.original.id}`} passHref>
-						<S.NameWrapper>
-							{startsWithHttp(row.original.image) ? (
-								<Image src={row.original.image} alt="" width={24} height={24} />
-							) : (
-								<div
-									style={{
-										width: '24px',
-										height: '24px',
-										background: '#8f8e8e',
-										textAlign: 'center',
-										color: '#fff',
-									}}
-								>
-									?
-								</div>
-							)}
-							<div style={{ display: 'flex', alignItems: 'center' }}>
-								<S.CoinName>{getValue<string>()}</S.CoinName>
-								<S.CoinSymbol>{row.original.symbol}</S.CoinSymbol>
-							</div>
-						</S.NameWrapper>
-					</Link>
-				</S.LinkWrapper>
-			),
-			size: 225,
-			textAlign: 'start',
-		},
-		{
-			header: 'Price',
-			accessorKey: 'current_price',
-			size: 100,
-			cell: ({ getValue }) => <div>${formatPrice(getValue<number>())}</div>,
-		},
-
-		{
-			header: '1h%',
-			accessorKey: 'price_change_percentage_1h_in_currency',
-			cell: ({ getValue }) => <PercentageChange value={getValue<number>()} />,
-			size: 85,
-		},
-
-		{
-			header: '24h%',
-			accessorKey: 'price_change_percentage_24h_in_currency',
-			cell: ({ getValue }) => <PercentageChange value={getValue<number>()} />,
-			size: 85,
-		},
-
-		{
-			header: '7d%',
-			accessorKey: 'price_change_percentage_7d_in_currency',
-			cell: ({ getValue }) => <PercentageChange value={getValue<number>()} />,
-			size: 85,
-		},
-		{
-			header: 'Market Cap',
-			accessorKey: 'market_cap',
-			size: 150,
-			cell: ({ getValue }) => (
-				<div>${formatLargeValue(getValue<number>())}</div>
-			),
-		},
-		{
-			header: 'Volume (24h)',
-			accessorKey: 'total_volume',
-			size: 150,
-			cell: ({ getValue }) => (
-				<div>${formatLargeValue(getValue<number>())}</div>
-			),
-		},
-		{
-			header: 'Circulating Supply',
-			accessorKey: 'circulating_supply',
-			cell: ({ row, getValue }) => (
-				<div>
-					<span>{formatLargeValue(getValue<number>())}</span>{' '}
-					<span style={{ textTransform: 'uppercase' }}>
-						{row.original.symbol}
-					</span>
-				</div>
-			),
-			size: 150,
-		},
-		{
-			header: 'Last 7 days',
-			accessorFn: (row) => row.sparkline_in_7d.price,
-			cell: ({ row, getValue }) => (
-				<Sparklines data={getValue<number[]>()} width={164} height={53}>
-					<SparklinesLine
-						color={
-							Number(
-								row.original.price_change_percentage_7d_in_currency
-									? row.original.price_change_percentage_7d_in_currency.toFixed(
-											2
-									  )
-									: 0
-							) > 0
-								? upColor
-								: downColor
+	const columns = useMemo<TableColumn<CoinOnWatchlist | CoinData>[]>(
+		() => [
+			{
+				id: 'add-to-watchlist',
+				header: '',
+				cell: ({ row }) => (
+					<WatchlistButton
+						coinId={row.original.id}
+						isOnWatchlist={
+							'isOnWatchlist' in row.original
+								? row.original.isOnWatchlist
+								: false
 						}
-						style={{ fill: 'none', strokeWidth: '2px' }}
+						watchlistCallback={refetch}
 					/>
-				</Sparklines>
-			),
-			minSize: 184,
-		},
-	];
+				),
+
+				size: 'auto' as unknown as number,
+			},
+			{
+				header: '#',
+				accessorKey: 'market_cap_rank',
+				size: 50,
+				textAlign: 'start',
+			},
+			{
+				header: 'Name',
+				accessorKey: 'name',
+				cell: ({ row, getValue }) => (
+					<S.LinkWrapper>
+						<Link href={`/coins/${row.original.id}`} passHref>
+							<S.NameWrapper>
+								{startsWithHttp(row.original.image) ? (
+									<Image
+										src={row.original.image}
+										alt=""
+										width={24}
+										height={24}
+									/>
+								) : (
+									<div
+										style={{
+											width: '24px',
+											height: '24px',
+											background: '#8f8e8e',
+											textAlign: 'center',
+											color: '#fff',
+										}}
+									>
+										?
+									</div>
+								)}
+								<div style={{ display: 'flex', alignItems: 'center' }}>
+									<S.CoinName>{getValue<string>()}</S.CoinName>
+									<S.CoinSymbol>{row.original.symbol}</S.CoinSymbol>
+								</div>
+							</S.NameWrapper>
+						</Link>
+					</S.LinkWrapper>
+				),
+				size: 225,
+				textAlign: 'start',
+			},
+			{
+				header: 'Price',
+				accessorKey: 'current_price',
+				size: 100,
+				cell: ({ getValue }) => <div>${formatPrice(getValue<number>())}</div>,
+			},
+
+			{
+				header: '1h%',
+				accessorKey: 'price_change_percentage_1h_in_currency',
+				cell: ({ getValue }) => <PercentageChange value={getValue<number>()} />,
+				size: 85,
+			},
+
+			{
+				header: '24h%',
+				accessorKey: 'price_change_percentage_24h_in_currency',
+				cell: ({ getValue }) => <PercentageChange value={getValue<number>()} />,
+				size: 85,
+			},
+
+			{
+				header: '7d%',
+				accessorKey: 'price_change_percentage_7d_in_currency',
+				cell: ({ getValue }) => <PercentageChange value={getValue<number>()} />,
+				size: 85,
+			},
+			{
+				header: 'Market Cap',
+				accessorKey: 'market_cap',
+				size: 150,
+				cell: ({ getValue }) => (
+					<div>${formatLargeValue(getValue<number>())}</div>
+				),
+			},
+			{
+				header: 'Volume (24h)',
+				accessorKey: 'total_volume',
+				size: 150,
+				cell: ({ getValue }) => (
+					<div>${formatLargeValue(getValue<number>())}</div>
+				),
+			},
+			{
+				header: 'Circulating Supply',
+				accessorKey: 'circulating_supply',
+				cell: ({ row, getValue }) => (
+					<div>
+						<span>{formatLargeValue(getValue<number>())}</span>{' '}
+						<span style={{ textTransform: 'uppercase' }}>
+							{row.original.symbol}
+						</span>
+					</div>
+				),
+				size: 150,
+			},
+			{
+				header: 'Last 7 days',
+				accessorFn: (row) => row.sparkline_in_7d.price,
+				cell: ({ row, getValue }) => (
+					<Sparklines data={getValue<number[]>()} width={164} height={53}>
+						<SparklinesLine
+							color={
+								Number(
+									row.original.price_change_percentage_7d_in_currency
+										? row.original.price_change_percentage_7d_in_currency.toFixed(
+												2
+										  )
+										: 0
+								) > 0
+									? upColor
+									: downColor
+							}
+							style={{ fill: 'none', strokeWidth: '2px' }}
+						/>
+					</Sparklines>
+				),
+				minSize: 184,
+			},
+		],
+		[]
+	);
 
 	return coins && <Table columns={columns} data={coins} />;
 };
