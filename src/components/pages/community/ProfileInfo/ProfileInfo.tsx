4ad43ea14d186a36e91b71f9c9b6224cadfad88e
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-03-18 10:18:04 +0100

    Updated ProfileInfo values

--- src/components/pages/community/ProfileInfo/ProfileInfo.tsx
+++ src/components/pages/community/ProfileInfo/ProfileInfo.tsx
@@ -65,8 +65,8 @@ const ProfileInfo = () => {
 				</ImageWrapper>
 				<ProfileContent>
 					<ProfileNameWrapper>
-						<ProfileDisplayName>{user.name}</ProfileDisplayName>
-						<ProfileName>@displayName</ProfileName>
+						<ProfileDisplayName>{user.displayName}</ProfileDisplayName>
+						<ProfileName>{user.name}</ProfileName>
 						<ProfileStatisticWrapper>
 							<ProfileStatistic>
 								<StatisticNumber>0</StatisticNumber> Following
