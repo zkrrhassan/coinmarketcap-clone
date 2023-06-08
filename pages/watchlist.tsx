f649247256fd65a68b540e514d55ccd8e9e8693f
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-06-09 00:06:57 +0200

    Refactored watchlist page

--- pages/watchlist.tsx
+++ pages/watchlist.tsx
@@ -1,84 +1,37 @@
-import React, { useState } from 'react';
+import React, { MouseEvent, useState } from 'react';
 import { Container } from 'styled/elements/Container';
 import { useSession } from 'next-auth/react';
-import axios from 'axios';
-import { Watchlist } from '@prisma/client';
 import WatchlistList from 'components/pages/watchlist/WatchlistList/WatchlistList';
+import WatchlistModal from 'components/pages/watchlist/WatchlistModal/WatchlistModal';
 import WatchlistTable from 'components/pages/watchlist/WatchlistTable/WatchlistTable';
-import { useQuery } from '@tanstack/react-query';
+import { Watchlist } from '@prisma/client';
 
 const Watchlist = () => {
 	const { data: session } = useSession();
-	const userId = session?.user.id;
-	const [currentWatchlist, setCurrentWatchlist] = useState<Watchlist | null>(
-		null
-	);
-	const [coins, setCoins] = useState([]);
-	// WATCHLISTS QUERY
-	const { data: watchlists } = useQuery({
-		queryKey: ['watchlists', userId],
-		queryFn: async () =>
-			(
-				await axios.get<Watchlist[]>('/api/watchlist/getAll', {
-					params: {
-						userId: userId,
-					},
-				})
-			).data,
-		onSuccess(data) {
-			const mainWatchlist = data.find((watchlist) => watchlist.isMain === true);
-			setCurrentWatchlist(mainWatchlist!);
-		},
-		enabled: !!userId,
-		refetchOnWindowFocus: false,
-	});
-	// COINS ON WATCHLIST QUERY
-	useQuery({
-		queryKey: ['coins', currentWatchlist],
-		queryFn: async () => {
-			if (currentWatchlist?.coinIds.length === 0) return null;
+	const [modalOpen, setModalOpen] = useState<boolean>(false);
+	const [watchlistAction, setWatchlistAction] = useState<
+		'edit' | 'create' | null
+	>(null);
+	const [currentWatchlist, setCurrentWatchlist] = useState<Watchlist>();
 
-			return (
-				await axios.get(`https://api.coingecko.com/api/v3/coins/markets`, {
-					params: {
-						vs_currency: 'usd',
-						ids: currentWatchlist?.coinIds.join(','),
-						order: 'market_cap_desc',
-						per_page: 100,
-						sparkline: 'true',
-						price_change_percentage: '1h,24h,7d',
-					},
-				})
-			).data;
-		},
-		onSuccess: (data) => setCoins(data),
-		enabled: !!currentWatchlist,
-		refetchOnWindowFocus: false,
-	});
-	// UPDATE CURRENT WATCHLIST (after coin remove)
-	const { refetch } = useQuery({
-		queryKey: ['getWatchlist'],
-		queryFn: async () =>
-			(
-				await axios.get<Watchlist>('api/watchlist/get', {
-					params: {
-						watchlistId: currentWatchlist!.id,
-					},
-				})
-			).data,
-		onSuccess: (data) => setCurrentWatchlist(data),
-		refetchOnWindowFocus: false,
-		enabled: false,
-	});
+	const updateCurrentWatchlist = (watchlist: Watchlist) => {
+		setCurrentWatchlist(watchlist);
+	};
 
-	const changeCurrentWatchlist = (name: string) => {
-		if (!watchlists) return;
-		const newWatchlist = watchlists.find(
-			(watchlist) => watchlist.name === name
-		);
-		if (newWatchlist) {
-			setCurrentWatchlist(newWatchlist);
-		}
+	const handleOpenCreate = () => {
+		setModalOpen(true);
+		setWatchlistAction('create');
+	};
+
+	const handleModalClose = () => {
+		setModalOpen(false);
+		setWatchlistAction(null);
+	};
+
+	const handleOpenEdit = (e: MouseEvent) => {
+		e.stopPropagation();
+		setModalOpen(true);
+		setWatchlistAction('edit');
 	};
 
 	return (
@@ -90,15 +43,25 @@ const Watchlist = () => {
 				</>
 			) : (
 				<>
-					{watchlists && currentWatchlist && (
-						<WatchlistList
-							currentWatchlist={currentWatchlist}
-							watchlists={watchlists}
-							changeWatchlistCallback={changeCurrentWatchlist}
-						/>
-					)}
-					{coins && (
-						<WatchlistTable coins={coins} watchlistCallback={refetch} />
+					<WatchlistList
+						currentWatchlist={currentWatchlist}
+						setCurrentWatchlist={setCurrentWatchlist}
+						handleOpenCreate={handleOpenCreate}
+						handleOpenEdit={handleOpenEdit}
+					/>
+					{currentWatchlist && (
+						<>
+							<WatchlistModal
+								handleModalClose={handleModalClose}
+								modalOpen={modalOpen}
+								watchlistAction={watchlistAction}
+								currentWatchlist={currentWatchlist}
+							/>
+							<WatchlistTable
+								currentWatchlist={currentWatchlist}
+								updateCurrentWatchlist={updateCurrentWatchlist}
+							/>
+						</>
 					)}
 				</>
 			)}
